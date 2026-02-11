c832648da5afa75b062ef5a94eaadf98484cfd62
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-02-11 16:25:52 +0700

    add DataTypes class with user input handling for test cases

--- week1/DataTypes.java
+++ week1/DataTypes.java
@@ -1,5 +1,38 @@
 package week1;
+import java.util.Scanner;
 
 public class DataTypes {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] kasusuji = new int[100];
+        System.out.print("Masukkan Jumlah Kasus Uji: ");
+        int jmluji = input.nextInt();
+        
+        for (int i = 0; i < jmluji; i++){
+            System.out.print("Masukkan Kasus Uji: ");
+            kasusuji[i] = input.nextInt();
+        }
+
+        System.out.println("\nHasil Kasus Uji:");
+        for (int i = 0; i < jmluji;i++){
+            if(kasusuji[i] == byte.min_VALUE || kasusuji[i] == byte.max_VALUE){
+                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* byte");
+            }
+            else if(kasusuji[i] == short.min_VALUE || kasusuji[i] == short.max_VALUE){
+                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* short");
+            }
+            else if(kasusuji[i] == int.min_VALUE || kasusuji[i] == int.max_VALUE){
+                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* int");
+            }
+            else if(kasusuji[i] == long.min_VALUE || kasusuji[i] == long.max_VALUE){
+                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* long");
+            }
+            else{
+                System.out.println(kasusuji[i] + " tidak dapat disimpan dalam
+        }
+
+        
+
+
+    }
 }
