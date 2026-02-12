ae3573b50caf4296255059b792e15ff4584f15f3
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-02-12 15:28:22 +0700

    refactor DataTypes class to use BigInteger for input handling and improve output formatting

--- week1/DataTypes.java
+++ week1/DataTypes.java
@@ -1,38 +1,37 @@
 package week1;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class DataTypes {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] kasusuji = new int[100];
+        BigInteger[] kasusuji = new BigInteger[100];
         System.out.print("Masukkan Jumlah Kasus Uji: ");
         int jmluji = input.nextInt();
         
         for (int i = 0; i < jmluji; i++){
             System.out.print("Masukkan Kasus Uji: ");
-            kasusuji[i] = input.nextInt();
+            kasusuji[i] = input.nextBigInteger();
         }
 
         System.out.println("\nHasil Kasus Uji:");
         for (int i = 0; i < jmluji;i++){
-            if(kasusuji[i] == byte.min_VALUE || kasusuji[i] == byte.max_VALUE){
-                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* byte");
+            if (kasusuji[i].compareTo(BigInteger.valueOf(Long.MIN_VALUE)) >= 0 && kasusuji[i].compareTo(BigInteger.valueOf(Long.MAX_VALUE)) <= 0){
+                System.out.println(kasusuji[i] + " can be fitted in:");
+                if (kasusuji[i].compareTo(BigInteger.valueOf(Byte.MIN_VALUE)) >= 0 && kasusuji[i].compareTo(BigInteger.valueOf(Byte.MAX_VALUE)) <= 0){
+                    System.out.println("* byte");
+                }
+                if (kasusuji[i].compareTo(BigInteger.valueOf(Short.MIN_VALUE)) >= 0 && kasusuji[i].compareTo(BigInteger.valueOf(Short.MAX_VALUE)) <= 0){
+                    System.out.println("* short");
+                }
+                if (kasusuji[i].compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) >= 0 && kasusuji[i].compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) <= 0){
+                    System.out.println("* int");
+                }
+                System.out.println("* long");
+            } else {
+                System.out.println(kasusuji[i] + " can't be fitted anywhere.");
             }
-            else if(kasusuji[i] == short.min_VALUE || kasusuji[i] == short.max_VALUE){
-                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* short");
-            }
-            else if(kasusuji[i] == int.min_VALUE || kasusuji[i] == int.max_VALUE){
-                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* int");
-            }
-            else if(kasusuji[i] == long.min_VALUE || kasusuji[i] == long.max_VALUE){
-                System.out.println(kasusuji[i] + " dapat disimpan dalam: \n* long");
-            }
-            else{
-                System.out.println(kasusuji[i] + " tidak dapat disimpan dalam
-        }
-
-        
-
-
     }
+    input.close();
+}    
 }
