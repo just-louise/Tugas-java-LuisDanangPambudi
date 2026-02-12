fe25382e8ac03b4bec4b19b6e82d977986b4c63b
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-02-12 16:46:19 +0700

    add MathClass implementation and screenshot of output

---
+++ week1/MathClass/MathClass.java
@@ -0,0 +1,9 @@
+package week1.MathClass;
+
+public class MathClass {
+    public static void main(String[] args) { 
+		double x = 92.98;
+		int nx = (int) Math.round(x);
+        System.out.println("The value of x is: " + nx);
+	}
+}
