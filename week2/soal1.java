147557831f63197ebbf631b2660eb87760df776b
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-02-19 21:45:55 +0700

    analisis program tambah fitur done!

---
+++ week2/soal1.java
@@ -0,0 +1,115 @@
+package week2;
+
+class Restaurant {
+    private String[] nama_makanan;
+    private double[] harga_makanan;
+    private int[] stok;
+    private static byte id = 0;
+
+    public Restaurant() {
+        nama_makanan = new String[10];
+        harga_makanan = new double[10];
+        stok = new int[10];
+    }
+
+    public void setNama_Makanan(String[] nama_makanan){
+        this.nama_makanan = nama_makanan;
+    }
+    public void setHarga_makanan(double[] harga_makanan){
+        for (double harga : harga_makanan) {
+            if (harga < 0) {
+                System.out.println("harga tidak valid!");
+            }
+        }
+        this.harga_makanan = harga_makanan;
+    }
+    public void setStok(int[] stok){
+        for (int stokbarang : stok) {
+            if (stokbarang < 0) {
+                System.out.println("stok tidak valid! stok tidak boleh minus");
+            }
+        }
+        this.stok = stok;
+    }
+    public void setId(byte id){
+        id = 0;
+    }
+    public String[] getNama_Makanan(){
+        return nama_makanan;
+    }
+    public double[] getHarga_Makanan(){
+        return harga_makanan;
+    }
+    public int[] getStok(){
+        return stok;
+    }
+    public byte getId(){
+        return id;
+    }
+
+public void tambahMenuMakanan(String nama, double harga, int stok) {
+    this.nama_makanan[id] = nama;
+    this.harga_makanan[id] = harga;
+    this.stok[id] = stok;
+}
+
+public void tampilMenuMakanan() {
+    for (int i = 0; i < id; i++) {
+        if (!isOutOfStock(i)) {
+            System.out.println(
+                nama_makanan[i] + "[" + stok[i] + "]" + "\tRp. " + harga_makanan[i]
+            );
+        }
+    }
+}
+
+public boolean isOutOfStock(int id) {
+    if (stok[id] == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+public static void nextId() {
+    id++;
+}
+public void pesanMakanan(int id, int jumlah){
+    if (id >= 0 ){
+        if (stok[id] >= jumlah) {
+            stok[id] -= jumlah;
+            System.out.println("Pesanan berhasil " + nama_makanan[id]);
+        }
+        else{
+            System.out.println("Pesanan tidak berhasil! stok: " + stok[id]);
+        }
+    }
+    else{
+        System.out.println("Pesanan Tidak Valid! Pesanan tidak ada di menu");
+    }
+}
+
+public class soal1 {
+    public static void main(String[] args) {
+            Restaurant restaurant = new Restaurant();
+           restaurant.tambahMenuMakanan("Mie Ayam", 10000, 20);
+           nextId();
+           restaurant.tambahMenuMakanan("Nasi Goreng", 15000, 15);
+           nextId();
+           restaurant.tambahMenuMakanan("Ramen", 20000, 10);
+           nextId();
+           restaurant.tampilMenuMakanan();
+           System.out.println();
+           restaurant.pesanMakanan(0, 5);
+           restaurant.pesanMakanan(1, 11);
+           restaurant.pesanMakanan(2, 7);
+           System.out.println();
+           restaurant.tampilMenuMakanan();
+           System.out.println();
+           System.out.println("cek jika pesanan melebihi stok");
+           restaurant.pesanMakanan(0, 30);
+           restaurant.pesanMakanan(1, 56);
+           restaurant.pesanMakanan(2, 30);
+        }
+    }
+}
