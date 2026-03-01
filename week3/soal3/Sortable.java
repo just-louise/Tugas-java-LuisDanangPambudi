f6dec4db74bb3f4fcb63422cda5086a486857f2a
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-03-01 14:58:04 +0700

    selesai mengerjakan soal 3 week3

---
+++ week3/soal3/Sortable.java
@@ -0,0 +1,9 @@
+package week3.soal3;
+
+public abstract class Sortable {
+    public abstract int compare(Sortable b);
+     public static void shell_sort(Sortable[] a) {
+        // Shell sort body
+    }
+
+}
