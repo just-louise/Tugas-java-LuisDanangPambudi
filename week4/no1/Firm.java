6a829c84d7584767234db9d04a3557cff211216a
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-03-04 13:22:31 +0700

    refactor: improve formatting and structure of employee management classes

--- week4/no1/Firm.java
+++ week4/no1/Firm.java
@@ -2,11 +2,11 @@ package week4.no1;
 
 public class Firm {
     //-----------------------------------------------------------------
-// Creates a staff of employees for a firm and pays them.
-//-----------------------------------------------------------------
-public static void main (String[] args)
-{
-Staff personnel = new Staff();
-personnel.payday();
-}
+    // Creates a staff of employees for a firm and pays them.
+    //-----------------------------------------------------------------
+    public static void main (String[] args)
+    {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
 }
