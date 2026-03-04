f9a412a23014b44f37a009eb5bca0739749f1fd7
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-03-04 13:17:52 +0700

    implement employee management system with staff, employee, executive, hourly, and volunteer classes

--- week3/soal3/Sortable.java
+++ week3/soal3/Sortable.java
@@ -1,9 +1,7 @@
 package week3.soal3;
-
 public abstract class Sortable {
     public abstract int compare(Sortable b);
      public static void shell_sort(Sortable[] a) {
         // Shell sort body
     }
-
 }
