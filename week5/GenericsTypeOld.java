b0bf43dce91c9706ad92827de2bf353ee5a232af
Luis Danang Pambudi <luis.danang.tif425@polban.ac.id>
2026-03-13 21:59:02 +0700

    week5

---
+++ week5/GenericsTypeOld.java
@@ -0,0 +1,20 @@
+package week5;
+
+public class GenericsTypeOld {
+
+    private Object t;
+
+    public Object get() {
+        return t;
+    }
+
+    public void set(Object t) {
+        this.t = t;
+    }
+
+    public static void main(String args[]) {
+        GenericsTypeOld type = new GenericsTypeOld();
+        type.set("Java");
+        String str = (String) type.get();
+    }
+}
